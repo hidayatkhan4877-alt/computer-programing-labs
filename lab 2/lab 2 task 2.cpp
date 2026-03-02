79e1d0b7166b79c3af98b2edfa6b311473eadbc0
Hidayat-ullah <hidayatkhan4877@gmail.com>
2026-03-02 09:06:58 -0800

    Add files via upload

---
+++ lab 2/lab 2 task 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>   // iostream library include karta hai jo cout aur endl ke liye zaroori hai
+
+using namespace std;  // std namespace use karta hai taa ke cout aur endl directly likh saken
+
+int main()            // main function jahan se program start hota hai
+{
+    float pi = 3.14;  // float type ka variable 'pi' declare kiya aur usme 3.14 store kiya
+
+    // pi ki value screen par print kar raha hai
+    cout << "Value of pi is: " << pi << endl;
+
+    return 0;         // program ko successfully end karta hai
+}
